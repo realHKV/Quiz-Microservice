4bd1595e2a85d9b9a810053ec681da69d5968a94
realHKV <imnotharsh14@gmail.com>
2025-09-21 20:40:50 +0530

    added quizapp to monolith and gitignore git push origin main

---
+++ monolithic/quizApp/src/main/java/com/example/quizApp/dao/QuizDao.java
@@ -0,0 +1,8 @@
+package com.example.quizApp.dao;
+
+
+import com.example.quizApp.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz,Integer> {
+}
